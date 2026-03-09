68fb0baddb374a552d5fc6862e9408183d90b040
Gaurav Garg <gaugarg@nvidia.com>
2026-03-09 13:34:37 +0530

    Change CMake to use NCCL on Windows
    (cherry picked from commit 6c90394444d9b1628b59570e0bbdb3c7b6a89ce1)
    (cherry picked from commit 23e1600c7dcf6c87f2e2a9f1d7d96ee849e8ec9c)
    (cherry picked from commit 530aac6efb31aad61a021953238e050c34922d14)

--- ggml/cmake/FindNCCL.cmake
+++ ggml/cmake/FindNCCL.cmake
@@ -4,33 +4,71 @@
 
 find_path(NCCL_INCLUDE_DIR
     NAMES nccl.h
-    HINTS ${NCCL_ROOT} $ENV{NCCL_ROOT} $ENV{CUDA_HOME} /usr/local/cuda
+    HINTS ${NCCL_ROOT} $ENV{NCCL_ROOT} $ENV{CUDA_HOME} /usr/local/cuda $ENV{CUDA_PATH}
     PATH_SUFFIXES include
 )
 
-find_library(NCCL_LIBRARY
-    NAMES nccl
-    HINTS ${NCCL_ROOT} $ENV{NCCL_ROOT} $ENV{CUDA_HOME} /usr/local/cuda
-    PATH_SUFFIXES lib lib64
-)
+if(WIN32)
+    find_library(NCCL_STATIC_LIBRARY
+        NAMES nccl_static
+        HINTS ${NCCL_ROOT} $ENV{NCCL_ROOT} $ENV{CUDA_PATH} $ENV{CUDA_HOME}
+        PATH_SUFFIXES lib lib/x64
+    )
+
+    find_library(NCCL_DEVICE_LIBRARY
+        NAMES nccl_device
+        HINTS ${NCCL_ROOT} $ENV{NCCL_ROOT} $ENV{CUDA_PATH} $ENV{CUDA_HOME}
+        PATH_SUFFIXES lib lib/x64
+    )
+else()
+    find_library(NCCL_LIBRARY
+        NAMES nccl
+        HINTS ${NCCL_ROOT} $ENV{NCCL_ROOT} $ENV{CUDA_HOME} /usr/local/cuda
+        PATH_SUFFIXES lib lib64
+    )
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NCCL
-    DEFAULT_MSG
-    NCCL_LIBRARY NCCL_INCLUDE_DIR
-)
+
+if(WIN32)
+    find_package_handle_standard_args(NCCL
+        DEFAULT_MSG
+        NCCL_STATIC_LIBRARY NCCL_DEVICE_LIBRARY NCCL_INCLUDE_DIR
+    )
+else()
+    find_package_handle_standard_args(NCCL
+        DEFAULT_MSG
+        NCCL_LIBRARY NCCL_INCLUDE_DIR
+    )
+endif()
 
 if(NCCL_FOUND)
-    set(NCCL_LIBRARIES ${NCCL_LIBRARY})
+    if(WIN32)
+        set(NCCL_LIBRARIES ${NCCL_STATIC_LIBRARY} ${NCCL_DEVICE_LIBRARY})
+    else()
+        set(NCCL_LIBRARIES ${NCCL_LIBRARY})
+    endif()
     set(NCCL_INCLUDE_DIRS ${NCCL_INCLUDE_DIR})
 
     if(NOT TARGET NCCL::NCCL)
-        add_library(NCCL::NCCL UNKNOWN IMPORTED)
-        set_target_properties(NCCL::NCCL PROPERTIES
-            IMPORTED_LOCATION "${NCCL_LIBRARY}"
-            INTERFACE_INCLUDE_DIRECTORIES "${NCCL_INCLUDE_DIR}"
-        )
+        if(WIN32)
+            add_library(NCCL::NCCL INTERFACE IMPORTED)
+            set_target_properties(NCCL::NCCL PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${NCCL_INCLUDE_DIR}"
+                INTERFACE_LINK_LIBRARIES "${NCCL_STATIC_LIBRARY};${NCCL_DEVICE_LIBRARY}"
+            )
+        else()
+            add_library(NCCL::NCCL UNKNOWN IMPORTED)
+            set_target_properties(NCCL::NCCL PROPERTIES
+                IMPORTED_LOCATION "${NCCL_LIBRARY}"
+                INTERFACE_INCLUDE_DIRECTORIES "${NCCL_INCLUDE_DIR}"
+            )
+        endif()
     endif()
 endif()
 
-mark_as_advanced(NCCL_INCLUDE_DIR NCCL_LIBRARY)
+if(WIN32)
+    mark_as_advanced(NCCL_INCLUDE_DIR NCCL_STATIC_LIBRARY NCCL_DEVICE_LIBRARY)
+else()
+    mark_as_advanced(NCCL_INCLUDE_DIR NCCL_LIBRARY)
+endif()
